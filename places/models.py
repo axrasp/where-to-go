4ebd7f434f1ce00b2471c656cbc1bb61c286de4d
axrasp <aaraspopin@gmail.com>
2022-08-26 16:08:50 +0300

    add place model with datat

--- places/models.py
+++ places/models.py
@@ -3,27 +3,30 @@ from django.db import models
 # Create your models here.
 
 
-class Place (models.Model):
+class Place(models.Model):
     title = models.CharField(
-    'Название',
-    max_length=200,
-    blank=False,
-    ),
+        'Название',
+        max_length=200,
+        blank=False,
+    )
     description_short = models.TextField(
         'Краткое описание',
         blank=True
-    ),
+    )
     description_long = models.TextField(
         'Полное описание html',
         blank=True
-    ),
+    )
     lng = models.CharField(
         'Долгота',
         max_length=20,
         blank=True
-    ),
+    )
     ltd = models.CharField(
         'Широта',
         max_length=20,
         blank=True
-    ),
+    )
+
+    def __str__(self):
+        return str(self.title)
