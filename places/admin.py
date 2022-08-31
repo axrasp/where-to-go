9f39436549421bbb69a9827ced479cd81edb4ee9
axrasp <aaraspopin@gmail.com>
2022-08-31 18:45:57 +0300

    inline added

--- places/admin.py
+++ places/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Place, Image
 
-# Register your models here.
 
-admin.site.register(Place)
+class PlaceImageInline(admin.TabularInline):
+    model = Image
+    raw_id_fields = ['place', ]
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [PlaceImageInline]
+
+
 admin.site.register(Image)
