03c9a5d82e53578e7bfacc85a21d0aac7513520b
axrasp <aaraspopin@gmail.com>
2022-09-01 11:45:33 +0300

    requirements updated

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env.str("SECRET_KEY", "CHANGE_ME")
 DEBUG = env.bool("DEBUG", True)
 
 ALLOWED_HOSTS = env.list(
-    "ALLOWED_HOSTS", [".localhost", "127.0.0.1", "[::1]", ".google.com"]
+    "ALLOWED_HOSTS", [".localhost", "127.0.0.1", "[::1]", ".pythonanywhere.com"]
 )
 
 
