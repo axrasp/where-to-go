5dbbbc25b0ed1978a17174ce01ac7e36e50a528f
axrasp <aaraspopin@gmail.com>
2022-09-02 14:21:26 +0300

    secret key added

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY", "CHANGE_ME")
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", True)
